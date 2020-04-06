b5b44cce0adf57d8859da192faf8b6616a125154
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-04-05 19:08:20 -0700

    added article_detail.html

--- samples/urls.py
+++ samples/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ArticleDetailView
 
 urlpatterns = [
+    path('article/<int:pk>/', ArticleDetailView.as_view(),
+         name='article_detail'),
     path('', HomePageView.as_view(), name='home'),
-    path('about', AboutPageView.as_view(), name='about')
+    path('about', AboutPageView.as_view(), name='about'),
 ]
--- samples/views.py
+++ samples/views.py
@@ -1,8 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Article
 
 
-class HomePageView(TemplateView):
+# class HomePageView(TemplateView):
+# template_name = 'home.html'
+
+class HomePageView(ListView):
+    model = Article
     template_name = 'home.html'
+    context_object_name = 'all_articles_list'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
 
 
 class AboutPageView(TemplateView):
