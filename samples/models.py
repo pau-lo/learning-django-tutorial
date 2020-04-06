d3adcbb51d946149929621636328e76f0619e2c4
Pablo ( Paulo )  Lopez <pau_lo_lop_ez@protonmail.com>
2020-04-06 16:44:04 -0700

    modified pipfile added Pillow and a media folder

--- samples/models.py
+++ samples/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    author = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, default=1,)
+    photo = models.ImageField(upload_to="gallery", default='eagle1.jpg')
+
+    def __str__(self):
+        return self.title
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
